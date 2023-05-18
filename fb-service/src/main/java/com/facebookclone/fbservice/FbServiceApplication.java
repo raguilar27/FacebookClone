08564ae54eff58e8bf008b4823f8ca069f1fa916
raguilar27 <rogelioaguilar208@gmail.com>
2023-05-17 23:19:29 -0700

    added facebook backend

---
+++ fb-service/src/main/java/com/facebookclone/fbservice/FbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.facebookclone.fbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbServiceApplication.class, args);
+	}
+
+}
---
+++ fb-service/src/test/java/com/facebookclone/fbservice/FbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facebookclone.fbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
